f47514500ba09a11c84d80dd3fd8232ab959cd59
viorica2020 <viorica.moldovanu.iucosoft@gmail.com>
2020-09-30 23:54:49 +0300

    fix: move js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+  })
